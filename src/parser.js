9796ef4e4b23a99dc6aeb707ef5168241a8b55f5
Gagan Jakhotiya <gagan@coverfoxmail.com>
2017-04-10 23:13:49 +0530

    Added field configuration getter

--- src/parser.js
+++ src/parser.js
@@ -11,16 +11,46 @@ export function setSchema(newSchema) {
     console.log('SET', (schema = {...newSchema}))
 }
 
-function getDataType(field, schema){
-    let schemaDataType = schema[field]
+function getFieldConfig(key, dataTypeSchema) {
+    let datatype = getDataType(dataTypeSchema)
+      , isMandatory = key.substr(-1) === '!'
+      , { string, number, ...nonPrimitivaDataTypes } = datatypes
+      , config = {
+          key: isMandatory ? key.substr(0, key.length - 1) : key,
+          mandatory: isMandatory,
+          datatype: datatype,
+      }
+      , children;
 
-    if (isString(schemaDataType)) {
+      if (typeof nonPrimitivaDataTypes[datatype] !== 'undefined') {
+        if (datatype === datatypes.object) {
+            children = Object.keys(dataTypeSchema)
+        } else {
+            let arrayOf = isArrayOf(dataTypeSchema)
+              , arrayDataType = getDataType(arrayOf);
+            if (typeof nonPrimitivaDataTypes[arrayDataType] !== 'undefined') {
+                children = Object.keys(arrayOf)
+            }
+        }
+      }
+
+      config = {
+          ...config,
+          children,
+          mandatoryChildren: children.filter(child => child.substr(-1) === '!')
+      }
+
+      return config
+}
+
+function getDataType(dataTypeSchema){
+    if (isString(dataTypeSchema)) {
         return datatypes.string
-    } else if (isNumber(schemaDataType)) {
+    } else if (isNumber(dataTypeSchema)) {
         return datatypes.number
-    } else if (isObject(schemaDataType)) {
+    } else if (isObject(dataTypeSchema)) {
         return datatypes.object
     } else {
-        return isArrayOf(schemaDataType)
+        return datatypes.array
     }
 }
