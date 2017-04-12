4e92dc450023e77d93d4464c9bcddc875e7a59b8
Gagan Jakhotiya <gagan@coverfoxmail.com>
2017-04-12 19:09:37 +0530

    server setup

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var app = express()
+
+app.use(express.static(__dirname + '/build'));
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+})
+
+app.listen(8080, function () {
+  console.log('Listening!')
+})
